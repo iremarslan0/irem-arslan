ed9f25e342ac2ee96f1ac8509c56be3efa9405dd
iremarslan0 <127770730+iremarslan0@users.noreply.github.com>
2023-04-19 18:43:29 +0300

    Add files via upload

---
+++ hafta3_yas hesaplama.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int yas1, yas2, yas3, toplam;
+    cout<<"yas1 girin";
+    cin>>yas1;
+    cout<<"yas2 girin";
+    cin>>yas2;
+    cout<<"yas girin";
+    cin>>yas3;
+    toplam= yas1+yas2+yas3;
+    cout<<"toplam="<<toplam<<endl;
+
+}

82d425b6e98ef056153168982574766d1f9433be
iremarslan0 <127770730+iremarslan0@users.noreply.github.com>
2023-04-19 18:41:11 +0300

    Add files via upload
    hafta3 dairenin alanını hesaplayan program

---
+++ hafta3_daire alan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+   float r;
+   float alan;
+   const double pi=3.14;
+   cout<<"yaricapi gir: ";
+   cin>>r;
+   alan=3.14*r*r;
+   cout<<"alan: "<<alan<<endl;
+
+}

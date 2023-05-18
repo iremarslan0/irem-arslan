aefaa090a7166dd5d848dcb0d26d635ccad774b4
iremarslan0 <127770730+iremarslan0@users.noreply.github.com>
2023-05-18 20:47:50 +0300

    Update hafta8_kodlayıcı.cpp

--- hafta8_kodlayıcı.cpp
+++ hafta8_kodlayıcı.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 int pi_sayisi = 3;
 
 class Havuz {
@@ -14,19 +15,19 @@ int main() {
     Havuz havuz1, havuz2;
     double yaricap1, yaricap2;
 
-    std::cout << "Birinci havuzun yaricapini gir: ";
-    std::cin >> yaricap1;
+    cout << "Birinci havuzun yaricapini gir: ";
+    cin >> yaricap1;
     havuz1.yaricap = yaricap1;
 
-    std::cout << "Ikinci havuzun yaricapini gir: ";
-    std::cin >> yaricap2;
+    cout << "Ikinci havuzun yaricapini gir: ";
+    cin >> yaricap2;
     havuz2.yaricap = yaricap2;
 
     double alan1 = havuz1.getAlan();
     double alan2 = havuz2.getAlan();
 
-    std::cout << "Birinci havuzun alani: " << alan1 << std::endl;
-    std::cout << "Ikinci havuzun alani: " << alan2 << std::endl;
+    cout << "Birinci havuzun alani: " << alan1 << endl;
+    cout << "Ikinci havuzun alani: " << alan2 << endl;
 
     return 0;
 }

db8993a4dc5203f5a966fadbcd1639311edeafc3
iremarslan0 <127770730+iremarslan0@users.noreply.github.com>
2023-05-18 19:34:57 +0300

    denetleyici
    kare şeklindeki iki bahçenin kenar bilgilerini
    ölçerek, bahçelerin alan ve çevresini hesaplayan
    bir program

---
+++ hafta8_denetleyici.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+class Bahce {
+  public:
+    float kenar;
+
+    void alanHesapla() {
+      alan = kenar * kenar;
+    }
+
+    void cevreHesapla() {
+      cevre = 4 * kenar;
+    }
+
+    void bilgileriGoster() {
+      std::cout << "Bahce Alani: " << alan << " birim kare" << std::endl;
+      std::cout << "Bahce Cevresi: " << cevre << " birim" << std::endl;
+    }
+
+  private:
+    float alan;
+    float cevre;
+};
+
+int main() {
+  Bahce bahce1, bahce2;
+
+  std::cout << "1. Bahçe Kenar Uzunluðunu Girin: ";
+  std::cin >> bahce1.kenar;
+
+  std::cout << "2. Bahçe Kenar Uzunluðunu Girin: ";
+  std::cin >> bahce2.kenar;
+
+  bahce1.alanHesapla();
+  bahce1.cevreHesapla();
+  bahce1.bilgileriGoster();
+
+  bahce2.alanHesapla();
+  bahce2.cevreHesapla();
+  bahce2.bilgileriGoster();
+
+  return 0;
+}

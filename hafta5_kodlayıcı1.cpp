16157427e8e536fc20dd196127ffe9071ace3076
iremarslan0 <127770730+iremarslan0@users.noreply.github.com>
2023-05-29 20:02:47 +0300

    Kodlayıcı
    Kodlayıcı
    Klavyeden girilen 10 tam sayının toplamını ekrana yazdıran kodu nasıl yazarsınız?

---
+++ hafta5_kodlayıcı1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+    int toplam=0;
+    for(int i=0;i<10;i++)
+    {
+        int sayi;
+        cin >> sayi;
+        toplam += sayi;
+
+    }
+     cout << "toplam:" << toplam;
+}
